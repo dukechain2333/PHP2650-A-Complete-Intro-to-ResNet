0ccbaef3e4091f027c7e5a20f1eaf7543690c1ba
William Qian <william.z.qian@gmail.com>
2024-05-03 10:46:42 -0400

    Update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>A Complete Introduction to ResNet</span>,
-  project: {
-    link: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
-  },
-  docsRepositoryBase: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
-  footer: {
-    text: 'PHP2650 Final Project: William Qian & Dingxuan Zhang',
-  },
+    logo: <span>A Complete Introduction to ResNet</span>,
+    project: {
+        link: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
+    },
+    docsRepositoryBase: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
+    footer: {
+        text: 'PHP2650 Final Project: William Qian & Dingxuan Zhang',
+    },
+    editLink: {
+        component: null,
+    },
+    feedback: {
+        content: null,
+    },
 }
 
 export default config
