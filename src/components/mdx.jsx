96c7d0fc5d69d78da5532e72174e1b89e48cd4c2
William Qian <william.z.qian@gmail.com>
2024-05-03 01:35:10 -0400

    Update basic layout and Misc part

--- src/components/mdx.jsx
+++ src/components/mdx.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { Feedback } from '@/components/Feedback'
 import { Heading } from '@/components/Heading'
 import { Prose } from '@/components/Prose'
 
@@ -14,7 +13,6 @@ export function wrapper({ children }) {
     <article className="flex h-full flex-col pb-10 pt-16">
       <Prose className="flex-auto">{children}</Prose>
       <footer className="mx-auto mt-16 w-full max-w-2xl lg:max-w-5xl">
-        <Feedback />
       </footer>
     </article>
   )
