1f9a8d1bcccf26a8a0646faa3808168fd9147333
William Qian <william.z.qian@gmail.com>
2024-05-03 02:28:06 -0400

    Update project to nextra

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>A Complete Introduction to ResNet</span>,
+  project: {
+    link: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
+  },
+  docsRepositoryBase: 'https://github.com/dukechain2333/PHP2650-A-Complete-Intro-to-ResNet',
+  footer: {
+    text: 'PHP2650 Final Project: William Qian & Dingxuan Zhang',
+  },
+}
+
+export default config
